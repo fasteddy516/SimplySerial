0275b659e3ab7c65333bbfcca7e0fab1ad44d8ce
Edward Wright <fasteddy@thewrightspace.net>
2019-06-10 11:48:26 -0400

    solution/project modifications in preparation for installer

--- SimplySerial/Properties/AssemblyInfo.cs
+++ SimplySerial/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimplySerial")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Windows console application for serial communications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("fasteddy516")]
 [assembly: AssemblyProduct("SimplySerial")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Edward Wright (fasteddy516)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
